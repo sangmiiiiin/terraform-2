c9ab94c7b0c5f7099597c7af6f6719ce26ff1960
“sangmiiiiiin” <kwk680037@gmail.com>
2024-12-08 20:53:41 +0900

    💩 생성한 VPC안에 EC2가 생성이 안되고 default subnet에 생성

--- main.tf
+++ main.tf
@@ -89,3 +89,56 @@ resource "aws_route_table_association" "ecr-pub-subnet-2c-rt-assoc" {
   subnet_id      = aws_subnet.ecr-public-subnet[1].id
   route_table_id = aws_route_table.ecr-igw-rt.id
 }
+
+# create instance on sangmin-vpc (bastion)
+data "aws_ami" "al-recent" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-20*-x86_64"]
+  }
+
+  owners = ["137112412989"]
+
+}
+
+# 보안 그룹 생성
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic and all outbound traffic"
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+# Ingress Rule: SSH 접속 허용 (사용자의 IP)
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4         = "0.0.0.0/0" # SSH를 허용할 IP
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+# Engress Rule: anywhere 허용
+resource "aws_vpc_security_group_egress_rule" "allow_all_ipv4" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4         = "0.0.0.0/0" # 모든 IPv4 트래픽 허용
+  # from_port         = 0
+  # to_port           = 0
+  ip_protocol = "-1" # 모든 프로토콜 허용
+}
+
+resource "aws_instance" "bastion" {
+  ami             = data.aws_ami.al-recent.id
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.allow_ssh.name]
+  key_name = "multi-key"
+  availability_zone = aws_subnet.ecr-public-subnet[0].availability_zone
+  tags = {
+    Name = "bastion"
+  }
+
+}
